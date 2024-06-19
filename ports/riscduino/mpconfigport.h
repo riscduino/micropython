7f80f8f3e06b9de608dccbb1cb2664e124c2c064
dineshannayya <dinesh.annayya@gmail.com>
2024-06-19 20:41:42 +0530

    first riscduino working

--- ports/riscduino/mpconfigport.h
+++ ports/riscduino/mpconfigport.h
@@ -4,7 +4,7 @@
 #define MICROPY_ENABLE_GC                       (1)
 #define MICROPY_HELPER_REPL                     (1)
 #define MICROPY_ERROR_REPORTING                 (MICROPY_ERROR_REPORTING_TERSE)
-#define MICROPY_FLOAT_IMPL                      (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_FLOAT_IMPL                      (0)
 
 // Fine control over Python builtins, classes, modules, etc.
 #define MICROPY_PY_ASYNC_AWAIT                  (0)
